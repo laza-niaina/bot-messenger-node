f1ef833bd63b44e4355e6808198fce87503423ba
Laza Niaina <97720707+laza-niaina@users.noreply.github.com>
2023-11-18 15:46:42 +0300

    Update VerifyEvent.js

--- src/Callback/VerifyEvent.js
+++ src/Callback/VerifyEvent.js
@@ -1,17 +1,11 @@
-const { Event } = require('events'); // Assurez-vous d'ajuster le chemin selon votre structure de dossier
+class VerifyEvent {
 
-class VerifyEvent extends Event {
   static NAME = "verify_event";
 
   constructor(verify) {
-    super();
     this.verify = verify;
   }
 
-  /**
-   * Get the value of verify
-   * @return {Verify}
-   */
   getVerify() {
     return this.verify;
   }
