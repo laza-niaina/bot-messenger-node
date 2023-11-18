42f5633d8ae5ae3ee233254cfe5ee6bab53e3340
Laza Niaina <97720707+laza-niaina@users.noreply.github.com>
2023-11-18 15:40:59 +0300

    Update AbstractElement.js

--- src/Model/Attachment/Template/AbstractElement.js
+++ src/Model/Attachment/Template/AbstractElement.js
@@ -1,63 +1,40 @@
 class AbstractElement {
-  /**
-   * @param {string} title
-   * @param {string|null} subtitle
-   * @param {string|null} imageUrl
-   */
+
   constructor(title, subtitle = null, imageUrl = null) {
+    // TemplateValidators::validateTitleSize($title);
+    // TemplateValidators::validateSubtitleSize($subtitle);
     this.title = title;
     this.subtitle = subtitle;
     this.imageUrl = imageUrl;
   }
 
-  /**
-   * @return {Object}
-   */
   jsonSerialize() {
     return {
       title: this.title,
       subtitle: this.subtitle,
-      image_url: this.imageUrl,
+      image_url: this.imageUrl
     };
   }
 
-  /**
-   * @return {string|null}
-   */
   getImageUrl() {
     return this.imageUrl;
   }
 
-  /**
-   * @return {string|null}
-   */
   getSubtitle() {
     return this.subtitle;
   }
 
-  /**
-   * @return {string}
-   */
   getTitle() {
     return this.title;
   }
 
-  /**
-   * @deprecated use the constructor argument instead
-   * @param {string|null} imageUrl
-   */
   setImageUrl(imageUrl) {
     this.imageUrl = imageUrl;
   }
 
-  /**
-   * @deprecated use the constructor argument instead
-   * @param {string|null} subtitle
-   */
   setSubtitle(subtitle) {
-    // Implement validation if needed for subtitle size
+    // TemplateValidators::validateSubtitleSize($subtitle);
+
     this.subtitle = subtitle;
   }
 }
-
-module.exports = AbstractElement;
